86d08bb9f59dbb8460128114a5937696632c0839
HubertJakubowski <145012072+HubertJakubowski@users.noreply.github.com>
2023-09-29 10:26:20 +0200

    Add files via upload

---
+++ Funkcja_algorytmu_wyszukiwania_binarnego.py.py
@@ -0,0 +1,25 @@
+def wyszukaj(tab, szukana):
+    l = 0
+    p = 15
+
+    while l <= p:
+        sr = (l + p) // 2
+
+        if tab[sr] == szukana:
+            return sr
+        elif tab[sr] > szukana:
+            p = sr - 1
+        else:
+            l = sr + 1
+
+    return -1
+
+tablica = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+szukana_liczba = int(input("Podaj liczbę: "))
+
+pozycja = wyszukaj(tablica, szukana_liczba)
+
+if pozycja != -1:
+    print(f"Liczba {szukana_liczba} jest w {pozycja}")
+else:
+    print("nie znalazlo")
