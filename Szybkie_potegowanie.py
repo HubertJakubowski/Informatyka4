0dac06035b68681fd7fe997193b351033901628a
HubertJakubowski <145012072+HubertJakubowski@users.noreply.github.com>
2023-10-16 09:32:59 +0200

    Add files via upload

---
+++ Szybkie_potegowanie.py
@@ -0,0 +1,16 @@
+def szybkie_potegowanie(a, n):
+    w = 1
+    while n > 0:
+        if n % 2 == 1:
+            w *= a
+        a *= a
+        n //= 2
+    return w
+
+a = int(input("Podaj podstawę: "))
+n = int(input("Podaj wykładnik: "))
+
+wynik = szybkie_potegowanie(a, n)
+print(f"{a} do potęgi {n} wynosi: {wynik}")
+
+    
